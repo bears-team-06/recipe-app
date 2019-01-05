a66a335653d91363a27de0a299a6b61b99ae9256
sharad jain <shjain@expedia.com>
2019-01-05 21:47:30 +0530

    /api/recipes should return transormed recipes

---
+++ src/model/Recipe.js
@@ -0,0 +1,10 @@
+class Recipe {
+    constructor(id, name, ingredients, instructions) {
+        this.id = id
+        this.name = name
+        this.ingredients = ingredients
+        this.instructions = instructions
+    }
+}
+
+module.exports = Recipe
