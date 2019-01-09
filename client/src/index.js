7613694244222f688d2f3ed421e31df0ef9b9f30
sharad jain <shjain@expedia.com>
2019-01-09 18:29:02 +0530

    eslint disable

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import style from './App.module.css'
+import style from './App.module.css' //eslint-disable-line no-unused-vars
 ReactDOM.render(<App />, document.getElementById('root'));
