6dcfb9200759594b3e04dabd81fc56626298d257
denibulkashvili <denibulkashvili@gmail.com>
2019-01-11 12:36:10 +0700

    Run lint-fixup

--- src/tranformers.test.js
+++ src/tranformers.test.js
@@ -1,8 +1,8 @@
-const mockedData = require('./Mock/Mock')
-const transformRecipeResponse = require('./transformers')
+const mockedData = require('./Mock/Mock');
+const transformRecipeResponse = require('./transformers');
 
 it('transforms recipe object correctly', () => {
-    let transformedObject = transformRecipeResponse(mockedData)
-    expect(transformedObject).toBeDefined
-    expect(transformedObject).toMatchSnapshot()
-})
+  const transformedObject = transformRecipeResponse(mockedData);
+  expect(transformedObject).toBeDefined;
+  expect(transformedObject).toMatchSnapshot();
+});
